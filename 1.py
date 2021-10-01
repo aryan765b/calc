4728b7e16f5e988db067e6b434e5f2d9b5bff2f4
ARY <72200502+Ironboyind7@users.noreply.github.com>
2021-10-01 21:22:53 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
-a = float(input("enter a"))
-b = float(input("enter b"))
-print("sum = ",a+b)
+c = input("enter all numbers with comma to seperate values").split(",")
+d = 0
+for i in c:
+  d += float(i)
+ print("sum = ",d)
